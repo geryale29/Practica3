4e3ddb4941a5154b746580a6501fe567746f6146
geryale29 <adaw22gale@inslaferreria.cat>
2023-03-20 18:57:27 +0100

    Inverteix fet

---
+++ mavenproject1/src/main/java/myutils/MainClass.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myutils;
+
+/**
+ *
+ * @author gerya
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(MyUtils.inverteix("Hola que tal como estas jajaja"));
+        
+    }
+    
+}
